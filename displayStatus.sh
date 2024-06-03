c0c0366684b6b6a17ff714672d427ace7377dfe4
FrenchMartini <143716587+FrenchMartini@users.noreply.github.com>
2024-06-04 00:00:54 +0530

    Add files via upload

--- displayStatus.sh
+++ displayStatus.sh
@@ -1,1 +1,29 @@
 #!/bin/bash 
+user=$(whoami)
+#First checking if user is Core 
+if [[ $user == "Core" ]]; then 
+    echo "User verified proceeding to show status !"
+else 
+    echo "Permission not granted"
+fi 
+
+declare -a taskcounter
+declare -a taskcompleted
+
+for mentee in "/home/Core/mentees"/*; do
+    while IFS= read -r line; do 
+        line=$(echo "$line" | xargs)
+        if  [[ "${line: -1}" == ":"]]; then 
+            domain="${line%:}"
+            continue 
+        fi 
+        IFS=":" read -r task status <<< "$line"
+        task=$(echo "$task" | xargs) 
+        status=$(echo "$status" | xargs)
+        if [ -z "$domain" ] || [ -z "$task" ] || [ -z "$status" ]; then
+        continue
+        fi 
+        
+    done <"/home/Core/mentees/$mentee"
+
+
