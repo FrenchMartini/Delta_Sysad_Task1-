83e18a11dc9440ccddf1f9e8f743285577dac412
FrenchMartini <143716587+FrenchMartini@users.noreply.github.com>
2024-06-04 04:08:49 +0530

    completed the script for now

--- displayStatus.sh
+++ displayStatus.sh
@@ -10,7 +10,7 @@ fi
 declare -a taskcounter
 declare -a taskcompleted
 lastchecktime=$(cat "/home/Core/lastchecktime.txt" 2>/dev/null || echo 0)
-latestsubmision=''
+latestsubmission=''
 currenttime=$(date +%s)
 
 for mentee in "/home/Core/mentees"/*; do
@@ -30,12 +30,24 @@ for mentee in "/home/Core/mentees"/*; do
         index="$domain $task"
         taskcounter["index"]=$((taskcounter["index"]+1))
         if [ "$status" == "y" ]; then 
-           taskcompleted["index"]=$((taskcompleted["index"]+1))
-           if [ $(stat -c %Y "/home/Core/mentees/$menteeName/$domain/$task
-           
-           
-        
-        
-    done <"/home/Core/mentees/$menteeName"
+           tasksubmitted["index"]=$((tasksubmitted["index"]+1))
+           if [ $(stat -c %Y "/home/Core/mentees/$menteeName/$domain/$task") -gt "lastchecktime" ]; then 
+               latestsubmission+="$menteeName submited $task in $domain\n"
+            fi
+        fi 
+    done <"/home/Core/mentees/$menteeName/task_submitted.txt"
+done 
+echo "Percentage of total submissions"
+for filter in "${!taskcounter[@]}"; do 
+    domain=$(echo "filter" | cut -d '' -f1)
+    task=$(echo "filter" |cut -d '' -f2)
+    total=$(taskcounter["filter"])
+    submitted=$(tasksubmited["filter"])
+    percentage=$((100*submitted/total))
+    echo " $domain $task : $perecentage submitted"
+done 
+echo -e "\nRecent Submissions"
+echo -e "$latestsubmission"
+    
 
 
