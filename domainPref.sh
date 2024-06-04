4cc8a55a5fc0753f01638b1de2804fdef95af144
Adithya <adithyadoha123@gmail.comgit config -- user.email adithyadoha123@gmail.comgit config --global user.email 143716587+AD1GHOstcode@users.noreply.github.com>
2024-06-05 03:47:08 +0530

    corrected dir paths

--- domainPref.sh
+++ domainPref.sh
@@ -15,17 +15,17 @@ if [ -d "/home/Core/mentees/$user" ]; then
     
     echo "Enter your domain preferences (1-3) in preferred order (e.g., web sysad app):"
     read -r domain1 domain2 domain3
-    echo "$domain1" > "$menteeHome/domain_pref.txt"
-    [ -n "$domain2" ] && echo "$domain2" >> "/home/Core/mentees/$menteeName/domain_pref.txt"
-    [ -n "$domain3" ] && echo "$domain3" >> "/home/Core/mentees/$menteeName/domain_pref.txt"
+    echo "$domain1" > "/home/Core/mentees/$user/domain_pref.txt"
+    [ -n "$domain2" ] && echo "$domain2" >> "/home/Core/mentees/$user/domain_pref.txt"
+    [ -n "$domain3" ] && echo "$domain3" >> "/home/Core/mentees/$user/domain_pref.txt"
 
     # Appending roll number and domains to core's mentees_domain.txt file
     echo "$menteeRollNumber $domain1 $domain2 $domain3" >> "home/Core/mentees_domain.txt"
 
     #First checking if the domain is non empty then creating required sub directory 
 
-    [ -n "$domain1" ] && mkdir -p "/home/Core/mentees/$menteeName/$domain1"
-    [ -n "$domain2" ] && mkdir -p "/home/Core/mentees/$menteeName/$domain2"
-    [ -n "$domain3" ] && mkdir -p "/home/Core/mentees/$menteeName/$domain3"
+    [ -n "$domain1" ] && mkdir -p "/home/Core/mentees/$user/$domain1"
+    [ -n "$domain2" ] && mkdir -p "/home/Core/mentees/$user/$domain2"
+    [ -n "$domain3" ] && mkdir -p "/home/Core/mentees/$user/$domain3"
 
 fi
