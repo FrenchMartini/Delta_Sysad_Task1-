ce87a7cdd387788b282bc97f09adc6dcaecbae81
Adithya <adithyadoha123@gmail.comgit config -- user.email adithyadoha123@gmail.comgit config --global user.email 143716587+AD1GHOstcode@users.noreply.github.com>
2024-06-12 01:11:38 +0530

    Made final changes to this file and finishing it for now

--- deRegister.sh
+++ deRegister.sh
@@ -9,9 +9,24 @@ awk '{print $1}' "/home/adithya/Desktop/Delta_Sysad/menteeDetails.txt" | tail -n
     fi 
 done 
 
-echo "Enter the domains you want to deregister  (e.g., web app )"
+echo "Enter domains"
 read domain1 domain2 domain3 
-#Removing domain dir for the domains entered by the user 
-[ -n "$domain1" ] && rmdir -r "/home/Core/mentees/$user/$domain1"
-[ -n "$domain2" ] && rmdir -r "/home/Core/mentees/$user/$domain2"
-[ -n "$domain3" ] && rmdir -r "/home/Core/mentees/$user/$domain3"
+
+[ -n "$domain1" ] && rm -r "/home/Core/mentees/$user/$domain1"
+[ -n "$domain2" ] && rm -r "/home/Core/mentees/$user/$domain2"
+[ -n "$domain3" ] && rm -r "/home/Core/mentees/$user/$domain3"
+
+echo "Removing directories of deregistered domains"
+
+# Had errors when i did it directly putting a pattern to make things generalized 
+pattern=""
+[ -n "$domain1" ] && pattern="$pattern/$domain1/d;"
+[ -n "$domain2" ] && pattern="$pattern/$domain2/d;"
+[ -n "$domain3" ] && pattern="$pattern/$domain3/d;"
+
+# If non empty then it deletes the required domain from the mentees domain file 
+if [ -n "$pattern" ]; then
+  sed -i "$pattern" "/home/adithya/Desktop/Task1Testing/Domains.txt"
+fi
+
+echo "Removed deregistered domains from domain.txt"
