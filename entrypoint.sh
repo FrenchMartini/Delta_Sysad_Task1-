ce815c557af8d2b87d5ca5e90acc936e6f927e5f
Adithya Nair <adithyadoha123@gmail.com>
2024-06-20 23:02:35 +0530

    added comments to the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#just a simple echo statement for the sake of entrypoint
 echo "Hello to the induction server. Please find the required files in your directory."
+#Replace current shell procees with what the cotainer is running 
 exec "$@"
